ce12adcc17edd9bbe413c55a589fd88535839a20
Nupur Singh <nupur.singh2410@gmail.com>
2024-08-19 23:45:36 +0530

    construct diff by backtracking

--- myers.py
+++ myers.py
@@ -21,17 +21,49 @@ def myers_diff(m, n):
             v[k]= x #the most progress we can make along this diagonal with current no. of edits
 
             if x>= len(m) and y>= len(n): #end of both strings
-                trace.append(v)
+                trace.append(v.copy())
                 return build_diff(m, n, trace) #reconstruct the diff using trace
             
-        trace.append(v)
+        trace.append(v.copy())
     return []
 
 def build_diff(a, b, trace):
+    edit_list= []
+    i= len(a)
+    j= len(b)
+    for d in range(len(trace)-1, -1, -1):  #iterate backwards through trace. So, we start to iterate
+        #from position len(trace)-1
+        v= trace[d] #dictionary of furthest point for edit dist
+        k= i-j #current diagonal
+        #we need to figure out which previous diagonal we came from
+        if k== -d or (k != d and v.get(k-1, 0) < v.get(k+1, 0)):
+            previous_k= k+1
+        else:
+            previous_k= k-1
+        previous_x= v.get(previous_k, 0) #x co-ord of prev diagonal
+        previous_y= previous_x-previous_k #y co-ord of prev diagonal
+        while i> previous_x and j> previous_y: #while we haven't yet reached prev co-ordinates
+            edit_list.append(('=', a[i-1]))
+            #move backwards
+            i= i-1
+            j= j-1
+        #if not at the start of the string
+        if d> 0:
+            if i> previous_x: #if we moved right, we have deleted from a
+                edit_list.append(('-', a[i-1]))
+                i= i-1
+            else:
+                #if we moved down, we have inserted in b
+                edit_list.append(('+', b[j-1]))
+                j= j-1
+
+        return edit_list
+
+
     return
 
 
-a= "abc"
-b= "abb"
+a= "apple"
+b= "dapple"
 
-myers_diff(a,b)
+print(myers_diff(a,b))
